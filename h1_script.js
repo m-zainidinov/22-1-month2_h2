2dc9f125afc89d89ebfa6c628a23b349550fb328
m-zainidinov <mr.zainidinov@gmail.com>
2024-02-12 22:27:53 +0600

    Homework 2

---
+++ h1_script.js
@@ -0,0 +1,11 @@
+// Домашнее задание № 2
+
+let sum = 0
+const array1 = [100, 55, 15, 300, 20]
+
+for (let i = 0; i < array1.length; i++) {
+    sum += array1[i]
+    console.log(sum); // Дополнительное домашнее задание
+}
+
+console.log('The sum is', sum); // Домашнее задание
